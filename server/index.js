f77c2fa90d52d0ad35417a049ba8e3b9a53b90e6
htchen <ht.chen@mail.utoronto.ca>
2019-12-26 16:16:04 -0500

    configurate concurrently

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ app.get('/api/getList', (req,res) => {
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname+'/../client/public/not-found.html'));
 });
 
 const port = process.env.PORT || 5000;
