7bc808cc603de0a7711385314475cd932f32f46e
椎名深雪 <xiaochun.tong@mail.utoronto.ca>
2019-12-28 19:48:09 -0500

    push

---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  JWT_SECRET: 'verysecretstring',
+  GOOGLE: {
+    GOOGLE_CLIENT_ID: '48536141752-fhirsi9pum3iecolfrnnbbina2e45ia8.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: '9Hny5-JFOzObC_17QsyZI6Os'
+  }
+}
