bfa81caae27c01c0040eeefdc2a6d1da4db933ab
htchen <ht.chen@mail.utoronto.ca>
2020-01-04 14:43:02 -0500

    notification redirect browserrouter forcerefresh

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   render() { 
     return (   
       <div className="App">
-        <BrowserRouter>
+        <BrowserRouter forceRefresh={true}>
           <Navbar />
           <div className="container">
             <Switch>
