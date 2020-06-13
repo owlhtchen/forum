8b33f2c35e18fc8f9f4319df30ca758bd785a926
chenhu84 <ht.chen@mail.utoronto.ca>
2020-06-13 18:35:39 -0400

    sign in looks ok

--- client/src/components/SignIn/Signin.js
+++ client/src/components/SignIn/Signin.js
@@ -23,9 +23,34 @@ class Signin extends Component {
 
     render() {
         return (
-          <div>
-
-          </div>
+            <div className="sign-div">
+                <div className="form-div">
+                    <form onChange={this.handleChange}
+                          onSubmit={this.handleSubmit} className="sign-form">
+                        <div className="sign-form__group">
+                            <label htmlFor="username" className="sign-form__label">Email</label>
+                            <input id="email" type="text" className="sign-form__input" required/>
+                        </div>
+                        <div className="sign-form__group">
+                            <label htmlFor="username" className="sign-form__label">Password</label>
+                            <input id="username" type="text" className="sign-form__input" required/>
+                        </div>
+                        <button className="sign-form__btn">
+                            Sign In &#8594;
+                        </button>
+                    </form>
+                    <hr />
+                    <h2>Or try three party sign in 👇</h2>
+                    <GoogleLogin
+                        className="google-btn"
+                        clientId="48536141752-fhirsi9pum3iecolfrnnbbina2e45ia8.apps.googleusercontent.com"
+                        buttonText="Google Login"
+                        onSuccess={this.responseGoogle}
+                        onFailure={this.responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />
+                </div>
+            </div>
         );
     }
 }
